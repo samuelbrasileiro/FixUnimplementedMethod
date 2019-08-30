5d2b5388cdd07e457bf3ddf1e6644a651484e201
Samuel Brasileiro <samuelbsantosn@gmail.com>
2019-08-30 14:51:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FixUnimplementedMethod
+ Ruby Application for fixing Merge's problem of Unimplemented Method
---
+++ main.rb
@@ -0,0 +1,177 @@
+require_relative 'UnimplementedMethodExtractor.rb'
+require_relative 'GitProject.rb'
+require_relative 'BCUnimplementedMethod.rb'
+require_relative 'FixUnimplementedMethod.rb'
+
+if ARGV.length < 1
+  puts "invalid args, valid args example: "
+  puts "grumTreePath projectPath"
+  puts "projectPath is an optional param"
+  return
+end
+
+# test = FixUnimplementedMethod.new("sanity/quickml", "/home/arthurpires/Documents/faculdade/TAES/quickml",
+# "d1b6903a40c8cd359bcd02fc34b837f41f48f1e9", "src/main/java/quickdt/predictiveModels/decisionTree/TreeBuilder.java",
+# "attributeCharacteristics")
+# test.fix("buildTree", "")
+
+# test = FixUnimplementedMethod.new("sanity/quickml", "/home/arthurpires/Documents/faculdade/TAES/quickml",
+#   "d1b6903a40c8cd359bcd02fc34b837f41f48f1e9", "src/main/java/quickdt/predictiveModels/decisionTree/TreeBuilder.java",
+#   "attributeCharacteristics", 151)
+# content = File.read("/home/arthurpires/Documents/faculdade/TAES/fixPatternRequestUpdate/baseCommitClone/src/main/java/quickdt/predictiveModels/decisionTree/TreeBuilder.java")
+# test.getMethodName(content, 151)
+# return
+
+# Pre setup
+puts "Entry your password"
+password = STDIN.noecho(&:gets)
+
+gumTree = ARGV[0]
+
+if ARGV.length > 1
+  Dir.chdir(ARGV[1])
+end
+projectPath = Dir.getwd
+
+repLog = `#{"git config --get remote.origin.url"}`
+if repLog == ""
+  puts "invalid repository"
+  return
+end
+
+projectName = repLog.split("//")[1]
+projectName = projectName.split("github.com/").last.gsub("\n","").gsub(".git", "")
+commitHash = `#{"git rev-parse --verify HEAD"}`
+commitHash = commitHash.gsub("\n", "")
+#print commitHash
+print "\n"
+# Init  Analysis
+gitProject = GitProject.new(projectName, projectPath, "samuelbrasileiro", password)
+conflictResult = gitProject.conflictScenario(commitHash) #aqui vamos pegar o parentMerge
+#ESTRUTURA CR: [bool, [commits]]
+gitProject.deleteProject()
+
+#TIRAR ESSE#if conflictResult[0] #se existir 2 parents
+if conflictResult[0]
+  conflictParents = conflictResult[1] #conflictParents = parentMerge
+  #ESTRUTURA [PAI1,PAI2,FILHO]
+  travisLog = gitProject.getTravisLog(commitHash)#pegar a log do nosso commit
+
+  UnimplementedMethodExtractor = UnimplementedMethodExtractor.new()
+  unavailableResult = UnimplementedMethodExtractor.extractionFilesInfo(travisLog)
+
+
+  if unavailableResult[0] == "UnimplementedMethodVariable"
+    conflictCauses = unavailableResult[1]
+    ocurrences = unavailableResult[2]
+
+    bcUnimplementedMethod = BCUnimplementedMethod.new(gumTree, projectName, projectPath, commitHash,
+      conflictParents, conflictCauses)
+    bcUnSymbolResult = bcUnimplementedMethod.getGumTreeAnalysis()
+    puts "bcUNres = \n#{bcUnSymbolResult}\n"
+    if bcUnSymbolResult[0] != ""
+      baseCommit = bcUnSymbolResult[1]
+      cause = bcUnSymbolResult[0]
+      className = conflictCauses[0][0]
+      callClassName = conflictCauses[0][2]
+      methodNameByTravis = conflictCauses[0][1]
+      conflictFile = conflictCauses[0][3].tr(":","")
+      fileToChange = conflictFile.gsub(/\/home\/travis\/build\/[a-z|A-Z|0-9]+\/[a-z|A-Z|0-9]+\//,"")
+      conflictLine = Integer(conflictCauses[0][4].gsub("[","").gsub("]","").split(",")[0])
+
+
+      if className == callClassName
+        puts "A build Conflict was detect, the conflict type is " + unavailableResult[0] + "."
+        puts "Do you want fix it? Y or n"
+        resp = STDIN.gets()
+        # resp = "n"
+
+        puts ">>>>>>>>>>>>>>>class"
+        puts className
+        puts ">>>>>>>>>>>>>>>method"
+        puts methodNameByTravis
+
+        if resp != "n" && resp != "N"
+          fixer = FixUnimplementedMethod.new(projectName, projectPath, baseCommit, fileToChange, cause, conflictLine)
+          fixer.fix(className)
+        end
+      end
+
+
+      # TODO: get back deleted files
+      puts ">>>>>>>>>>>>>>>missing symbol"
+      puts cause
+      puts ">>>>>>>>>>>>>>>file "
+      puts conflictFile
+      puts ">>>>>>>>>>>>>>>fileToChange "
+      puts fileToChange
+      puts ">>>>>>>>>>>>>>>class"
+      puts className
+      puts ">>>>>>>>>>>>>>>method"
+      puts methodName
+      puts ">>>>>>>>>>>>>>>line"
+      puts conflictLine
+      puts ">>>>>>>>>>>>>>>base"
+      puts baseCommit
+    end
+  end
+
+  #TODO: METODO
+  if unavailableResult[0] == "UnimplementedMethodMethod"
+    conflictCauses = unavailableResult[1]
+    ocurrences = unavailableResult[2]
+
+    bcUnimplementedMethod = BCUnimplementedMethod.new(gumTree, projectName, projectPath, commitHash, conflictParents, conflictCauses)
+    bcUnSymbolResult = bcUnimplementedMethod.getGumTreeAnalysis()
+
+    #print("\nbcUnSymbolResult = \n#{bcUnSymbolResult}\n")
+    
+    if bcUnSymbolResult[0] != ""
+      baseCommit = bcUnSymbolResult[1]
+      cause = bcUnSymbolResult[0][0]
+      substituter = bcUnSymbolResult[0][1]#metodo identificado pelo log da gumtree
+      className = conflictCauses[0][0]
+      callClassName = conflictCauses[0][2]
+      methodNameByTravis = conflictCauses[0][1]#travis
+      conflictFile = conflictCauses[0][3].tr(":","")
+      fileToChange = conflictFile.gsub(/\/home\/travis\/build\/[^\/]+\/[^\/]+\//, "")
+      conflictLine = Integer(conflictCauses[0][4].gsub("[","").gsub("]","").split(",")[0])
+
+      print "class name / callclassname #{className}/ #{callClassName}\n\n\n"
+      if cause == methodNameByTravis
+        puts "A build Conflict was detect, the conflict type is " + unavailableResult[0] + "."
+        puts "Do you want fix it? Y or n"
+        resp = STDIN.gets()
+        # resp = "n"
+
+        puts ">>>>>>>>>>>>>>>class"
+        puts className
+        puts ">>>>>>>>>>>>>>>method"
+        puts methodNameByTravis
+        puts ">>>>>>>>>>>>>>>substituter"
+        puts substituter
+        if resp != "n" && resp != "N"
+          fixer = FixUnimplementedMethod.new(projectName, projectPath, baseCommit, fileToChange, cause, conflictLine, substituter)
+          fixer.fixMethod
+        end
+      end
+
+      puts ">>>>>>>>>>>>>>>missing symbol"
+      puts cause
+      puts ">>>>>>>>>>>>>>>file "
+      puts conflictFile
+      puts ">>>>>>>>>>>>>>>fileToChange "
+      puts fileToChange
+      puts ">>>>>>>>>>>>>>>class"
+      puts className
+      puts ">>>>>>>>>>>>>>>method"
+      puts methodNameByTravis
+      puts ">>>>>>>>>>>>>>>line"
+      puts conflictLine
+      puts ">>>>>>>>>>>>>>>base"
+      puts baseCommit
+    end
+  end
+end
+
+puts "FINISHED!"
